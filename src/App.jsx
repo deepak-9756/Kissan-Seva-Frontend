bae2bd539d20775178be99dd5e7bee2e4488f541
deepak-9756 <deepaksharma64349.a@gmail.com>
2024-01-12 15:11:14 +0530

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,16 @@ import SliderPanel from "./components/pages/SliderPanel";
 function App() {
   return (
     <>
-      <div className="bg-no-repeat   bg-cover bg-center bg-[url('/public/assets/images/background-image.avif')]">
-        <div className="pb-[5%] relative z-[12]">
+      <div className="bg-no-repeat  bg-cover bg-center bg-[url('/public/assets/images/background-image.avif')]">
+        <div className="pb-[6%] relative z-[12]">
           <Navbar />
+
+          <div className="fixed z-[5]  max-[480px]:mt-[20%] max-[600px]:mt-[14%] max-[1048px]:mt-[8%] mt-[7%]">
+            <Marquee />
+          </div>
         </div>
-        <div className="fixed z-[10] mt-[1%]">
-          <Marquee />
-        </div>
-        <div className="z-[10] p-[3%]">
+
+        <div className="z-[10] max-[480px]:p-[25%] max-[480px]:px-[10%] max-[600px]:p-[14%] max-[1048px]:p-[8%] p-[3%]">
           <SliderPanel />
           <FarmerRegistration />
         </div>
